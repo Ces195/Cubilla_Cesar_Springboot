542cbe3d16bd3501e628594ed451242e742c90e8
Ces195 <cubillac195@gmail.com>
2026-06-16 23:55:22 +0000

    mensaje del commit

---
+++ src/main/java/Cubilla_Cesar/Springboot/controller/LoginController.java
@@ -0,0 +1,13 @@
+package Cubilla_Cesar.Springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
